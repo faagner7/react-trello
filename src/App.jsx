9a104b59eb2c3b4c8b55c4bdb0ac2873f4f63780
Fagner Rodrigues <faagnerrodrigues@gmail.com>
2023-03-28 12:52:07 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Fagner from './Fagner'
+
+function App() {
+  return (
+    <div className='App'>
+      <Fagner />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Fagner.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+
+import React, { useState } from 'react'
+import Board from 'react-trello'
+
+import data from './data'
+
+function Fagner() {
+  const [state, setState] = useState({ boardData: data })
+
+  const handleDragStart = (cardId, laneId) => {
+    console.log('drag started')
+    console.log(`cardId: ${cardId}`)
+    console.log(`laneId: ${laneId}`)
+  }
+
+  const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
+    console.log('drag ended')
+    console.log(`cardId: ${cardId}`)
+    console.log(`sourceLaneId: ${sourceLaneId}`)
+    console.log(`targetLaneId: ${targetLaneId}`)
+  }
+
+  const setEventBus = (eventBus) => {
+    setState({ eventBus })
+  }
+
+  return (
+    <div className='App'>
+      <div className='App-header'>
+        <h3>React Trello Demo</h3>
+      </div>
+      <div className='App-intro'>
+        <Board
+          data={state?.boardData || { lanes: [] }}
+          draggable
+          hideCardDeleteIcon
+          eventBusHandle={setEventBus}
+          handleDragStart={handleDragStart}
+          handleDragEnd={handleDragEnd}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Fagner
